e2f75989327b01998ebec4204ff1f59e63f82350
wolfrayet <yvonne.shenlong@gmail.com>
2022-12-30 13:32:32 +0100

    update to obj

--- src/tensor.py
+++ src/tensor.py
@@ -7,33 +7,53 @@ import os, sys
 
 class Particles:
     def __init__(self, ngrid, L):
-        self.mesh = np.zeros((ngrid, ngrid, ngrid))
+        self.rho = np.zeros((ngrid, ngrid, ngrid))
+        self.phi = None
+        self.FFTphi = None
+        self.tensor = None
+
         self.ngrid = ngrid
         self.h = L/ngrid
         self.range = [[0,L],[0,L],[0,L]]
+        self.k = fftfreq(ngrid, d=self.h)[np.mgrid[0:ngrid,0:ngrid,0:ngrid]] * np.pi * 2
 
     def assign(self, pos, value):
         if isinstance(value, float):
             ret = binned_statistic_dd(pos, value*pos.shape[0], self.ngrid, range=self.range)
-        elif isinstance(value, np.ndarray) & (np.ndim(value) == 3):
-            ret = binned_statistic_dd(pos, value, self.ngrid, range=self.range)
+        elif isinstance(value, np.ndarray) & (np.ndim(value) == 1):
+            ret = binned_statistic_dd(pos, value, statistic='sum', bins=self.ngrid, range=self.range)
         else:
-            raise TypeError('value must be a float scalar or 3D numpy array')
-        self.mesh = ret.statistic / self.h**3
+            raise TypeError('value must be a float scalar or 1D numpy array')
+        self.rho = ret.statistic / self.h**3
     
     def FFTpotential(self, Rs, soft=1e-38):
-        k = fftfreq(self.ngrid, d=self.h)[np.meshgrid[0:self.ngrid,0:self.ngrid,0:self.ngrid]]
-        Ghat = -1. / (np.sum(k**2, axis=0) + soft)
+        Ghat = -1. / (np.sum(self.k**2, axis=0) + soft)
         Ghat[0,0,0] = 0.
-        return fourier_gaussian(fftn(self.mesh), sigma=Rs) * Ghat
+        self.FFTphi = fourier_gaussian(fftn(self.rho), sigma=Rs) * Ghat
 
-    def potential(self, Rs, soft=1e-38):
-        FFTphi = self.FFTpotential(Rs, soft=soft)
-        return ifftn(FFTphi).real
-        
-    def tensor(self, Rs, soft=1e-38):
-        k = fftfreq(self.ngrid, d=self.h)[np.meshgrid[0:self.ngrid,0:self.ngrid,0:self.ngrid]]
-        FFTphi = self.FFTpotential(Rs, soft=soft)
-        Hij = -np.einsum('iklm,jklm->ijklm', k, k)
-        return ifftn(FFTphi * Hij, axes=(2,3,4)).real
+    def PotentialField(self, Rs=1, soft=1e-38):
+        if self.FFTphi is None:
+            self.FFTpotential(Rs, soft=soft)
+        self.phi = ifftn(self.FFTphi)
 
+    def PotentialInterp(self, pos):
+        return
+    
+    def TensorField(self, Rs=1, soft=1e-38):
+        if self.FFTphi is None:
+            self.FFTpotential(Rs, soft)
+        Hij = -np.einsum('iklm,jklm->ijklm', self.k, self.k)
+        self.tensor = ifftn(self.FFTphi * Hij, axes=(2,3,4)).real
+
+    def TensorInterp(self, pos):
+        return 
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError(f'Use: python {sys.argv[0]} [file]')
+    
+    if not os.path.isfile(sys.argv[1]):
+        raise FileNotFoundError
+
+    hf = h5py.File(sys.argv[1], 'r')
+    
